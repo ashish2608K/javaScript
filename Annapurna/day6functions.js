a3b0511f94129f8b9a31ad3b303a9725cad8f6e4
Ashish shekhawat <ashishshekhawat2005@gmail.com>
2026-07-14 19:35:02 +0530

    functions

---
+++ Annapurna/day6functions.js
@@ -0,0 +1,37 @@
+/* What is a Function?
+Simple definition:
+
+A function is a reusable block of code that performs a specific task.
+
+Think of a function as a machine.
+
+You give it input → it does some work → it may give output.
+
+Example:
+Input
+  ↓
+Function
+  ↓
+Output
+Real website examples 🌐
+
+You click a button:
+
+Click Login Button
+        ↓
+loginUser() function runs
+        ↓
+Checks username/password
+        ↓
+Shows dashboard*/
+
+function interoduction(){
+    console.log("i am ashish")
+    console.log("i am a Poor progarmmer")
+    console.log("i want to join paraSH");
+    
+}
+
+interoduction();
+
+function 

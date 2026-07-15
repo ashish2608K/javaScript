ebbb806f59e001d73b626f4951ae2c59196ffc7f
Ashish shekhawat <ashishshekhawat2005@gmail.com>
2026-07-15 16:17:06 +0530

    new Exericese

--- Annapurna/Exericeses/exericese3.js
+++ Annapurna/Exericeses/exericese3.js
@@ -1,15 +1,32 @@
-const checkEligibility = function (name, age, runningTime) {
-    return (name, age, runningTime)
+// const checkEligibility = function (name, age, runningTime) {
 
-    if (age <= 20 && runningTime === "15 mins") {
-        console.log("You are Eligible")
+//     if (age <= 20 && runningTime <= 15) {
+//         return "Candidate: " + name + "\nStatus: Eligible";
+//     } else {
+//         return "Candidate: " + name + "\nStatus: Not Eligible";
+//     }
+
+// };
+
+// const result = checkEligibility("Ashish", 20, 13);
+
+// console.log(result);
+
+function calculateTicket(age){
+    if (age<12){
+        return "Ticket Price: ₹100"
     }
 
-    else {
-        console.log("You are not Eligible");
-
+    else if (age>=12 && age<=59){
+        return "Ticket Price: ₹200"
     }
-};
 
+    else if (age==60 || age>60){
+        return"Ticket price: ₹250"
+    }
+}
 
-checkEligibility("ashish",20,13);
+const result = calculateTicket(2);
+
+console.log(result);
+

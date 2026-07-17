2ed6b635da1106cb32a786caf3caa8cd8258e130
Ashish shekhawat <ashishshekhawat2005@gmail.com>
2026-07-17 22:17:17 +0530

    learning arrays

---
+++ Annapurna/day7arrays.js
@@ -0,0 +1,41 @@
+/*What is an Array?
+Simple definition:
+
+An array is a variable that stores multiple values in a single place.
+
+Instead of writing:
+
+const student1 = "Ashish";
+const student2 = "Komal";
+const student3 = "Rahul";
+const student4 = "Priya";
+
+We write:
+
+const students = ["Ashish", "Komal", "Rahul", "Priya"];
+
+Now one variable stores many values.*/
+
+// arrays can store anything
+
+const furits = ["KOMAL","mango","watermelon","strawberrys"];
+const numbera =[8448110734];
+const answers = [true, false, true];
+// const data =[
+//     "komal",
+//     8,
+//     true
+// ]
+
+function data(football,programmingLanguages,trecks){
+    footballClubs = ["Barcelona","Barcelona","Barcelona","always Barcelona"];
+    programmingLanguages = ["JavaScript","HTML","CSS","React"]
+    trecks = ["Annapurna","Rupin pass","Hampta pass"]
+}
+
+const result = data();
+console.log(result);
+
+
+
+

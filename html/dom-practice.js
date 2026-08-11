fb97b7359913ee2630acddfad57e2fe13d6be27b
Ashish shekhawat <ashishshekhawat2005@gmail.com>
2026-08-11 23:04:56 +0530

    iuoagytfcbnjk

--- html/dom-practice.js
+++ html/dom-practice.js
@@ -1,15 +1,14 @@
 const me = document.querySelector(".me");
 me.textContent = "I want to trek a lot. I want to see mountains. I want to live on mountains with myyy babydoll";
 
-const title = document.querySelector("#title");
+const title = document.querySelector(".title");
 const profession = document.querySelector("#profession");
-title.textContent = "JavaScript controls this page 😎"
+title.textContent = "JavaScript + DOM😎"
 profession.textContent = "PARA SF";
+title.classList.add("title")
 
-title.style.fontSize = "30px"
-title.style.color = "red";
-title.style.backgroundColor = "Black"
-title.style.padding = "40px"
-
-// console.log(nameProfession);
+// title.style.fontSize = "30px";
+// title.style.color = "red";
+// title.style.backgroundColor = "Black";
+// title.style.padding = "40px";
 

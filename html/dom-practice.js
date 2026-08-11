6163ba080d0b50dce3ab1b38eca82d69e22df581
Ashish shekhawat <ashishshekhawat2005@gmail.com>
2026-08-11 18:03:33 +0530

    soeufljasek

--- html/dom-practice.js
+++ html/dom-practice.js
@@ -1,3 +1,8 @@
 const me = document.querySelector(".me");
 me.textContent = "i want to trek a lot i want to see mountains , i want to live on mountains with myyy babydoll"
 console.log(me);
+
+const name = document.querySelector(".name")
+name.textContent = "Ashish"
+console.log(name);
+

7631bb06ee461f9925f1e689eee06d23e97296f6
Ashish shekhawat <ashishshekhawat2005@gmail.com>
2026-08-12 10:18:35 +0530

    ioudtyrhtefsdxcghj

--- html/dom-practice.js
+++ html/dom-practice.js
@@ -1,18 +1,13 @@
-// const me = document.querySelector(".me");
-// me.textContent = "I want to trek a lot. I want to see mountains. I want to live on mountains with myyy babydoll";
+const me = document.querySelector(".me");
+me.textContent = "I want to trek a lot. I want to see mountains. I want to live on mountains with myyy babydoll";
 
-// const title = document.querySelector(".title");
-// const profession = document.querySelector("#profession");
-// title.textContent = "JavaScript + DOM😎"
-// profession.textContent = "PARA SF";
-// title.classList.add("title")
+const title = document.querySelector(".body");
+const profession = document.querySelector("#profession");
+title.textContent = "JavaScript + DOM😎"
+profession.textContent = "PARA SF";
+title.classList.add("body")
 
 // title.style.fontSize = "30px";
 // title.style.color = "red";
 // title.style.backgroundColor = "Black";
 // title.style.padding = "40px";
-
-
-for ( let i = 1; i <=3000; i++ ){
-    console.log("I love you komu ",i);
-}

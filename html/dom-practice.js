8fc99dab8df12501baf279c912680e4dd15e25c4
Ashish shekhawat <ashishshekhawat2005@gmail.com>
2026-08-12 16:01:28 +0530

    EFRGETB

--- html/dom-practice.js
+++ html/dom-practice.js
@@ -1,7 +1,3 @@
-const me = document.querySelector(".me");
-me.textContent = "I want to trek a lot. I want to see mountains. I want to live on mountains with myyy babydoll";
-
-
 const button = document.querySelector(".dark_mode");
 button.addEventListener("click", () => {
     document.body.classList.toggle("dark_mode");
